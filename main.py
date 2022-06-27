e3e1abb3b44f365c3443d284bb144bf90b5b64e4
constantin-jehn@gmx.de <do00noto@aibe-hetep.aibe.uni-erlangen.de>
2022-06-27 17:24:09 +0200

    cst lr

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def optimize():
     src_folder = "sample_data"
     prep_folder = "cropped_images"
     src_folder = "sample_data"
-    result_string = "Ep_5_prereg_27_06_15_50"
+    result_string = "Ep_5_prereg_27_06_16:15"
     result_folder = os.path.join("results", result_string)
     tensor_board_folder = os.path.join("runs", result_string)
     
@@ -59,7 +59,7 @@ def optimize():
     sav_gol_order = 4
 
     #lambda function for setting learning rate
-    lambda1 = lambda epoch: [1,0.8,0.6,0.5,0.5][epoch] if epoch  < 5  else 0.2 if epoch < 10 else 0.125
+    lambda1 = lambda epoch: [1,1,1,1,1][epoch] if epoch  < 5  else 0.2 if epoch < 10 else 0.125
     #lambda1 = lambda epoch: 1 if epoch in [0] else 0.5 if epoch in [1] else 0.25 if epoch in [2,3,4] else 0.2
     #lambda1 = lambda epoch: 1 if epoch in [0] else 0.2
 
